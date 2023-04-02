1806488fd280c226072a4f505f6a47c01dd75d77
Duc Nguyen <nmducwin@gmail.com>
2023-04-02 14:35:48 +0700

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+alert("Welcome to calculator")
+function display(number){
+    //+= : cộng với một giá trị nào đó và lấy kết quả tổng của 2 giá trị
+    document.getElementById('result').value += number
+    return number
+}
+function solve(){
+    var a = document.getElementById('result').value;
+    // eval: lấy kết quả của một string
+    var b = eval(a);
+    document.getElementById('result').value = b;
+    return b;
+}
+function clearSCR(){
+    document.getElementById('result').value = ""
+}
